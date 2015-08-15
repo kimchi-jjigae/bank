ed68245ac9b8081d006537dfd50ae98d5039c635
Tobias Widlund <widlundtobias@gmail.com>
2015-08-15 21:32:10 +0200

    added child game

---
+++ src/intersector.cpp
@@ -0,0 +1,8 @@
+#include "intersector.hpp"
+
+bool intersects(const glm::uvec2& position, const fea::Quad& quad)
+{
+    glm::uvec2 qPos = (glm::uvec2)quad.getPosition();
+    glm::uvec2 size = (glm::uvec2)quad.getSize();
+    return position.x > qPos.x && position.y > qPos.y && position.x < qPos.x + size.x && position.y < qPos.y + size.y;
+}
