141fed2e08f763cdffa95beb61e85c840f65ce26
Kim Wong <kimspindel@gmail.com>
2015-08-16 13:49:31 +0200

    abandons hip

--- src/behaviouralstates/walkingbstate.cpp
+++ src/behaviouralstates/walkingbstate.cpp
@@ -1,4 +1,5 @@
 #include "walkingbstate.hpp"
+#include <math>
 #include "../global.hpp"
 
 WalkingBState::WalkingBState(fea::MessageBus& bus, glm::vec2 targetPos, float speed) :
@@ -23,6 +24,25 @@ void WalkingBState::update()
     glm::vec2 direction = mPositionDestination - charPos;
     direction = glm::normalize(direction);
     mVelocity = direction * mSpeed;
+
+    // working out the direction:
+    float angle = atan2(direction.y, direction.x);
+    if(angle >= -0.75f * glm::pi<float>() && angle < -0.25f * glm::pi<float>())
+    {
+        // downwards
+    }
+    else if(angle >= -0.25f * glm::pi<float>() && angle < 0.25f * glm::pi<float>())
+    {
+        // right
+    }
+    else if(angle >= 0.25f * glm::pi<float>() && angle < 0.75f * glm::pi<float>())
+    {
+        // upwards
+    }
+    else
+    {
+        // left
+    }
 
     glm::vec2 nextPos = charPos + mVelocity;
     mCharacter->setPosition(nextPos);
